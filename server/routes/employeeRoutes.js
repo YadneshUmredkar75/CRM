b415516a1182dc2e7ee1156cf5b896d53bae20c4
YadneshUmredkar75 <yadneshUmredkar75@gmail.com>
2025-11-28 18:10:32 +0530

    update router

--- server/routes/employeeRoutes.js
+++ server/routes/employeeRoutes.js
@@ -6,33 +6,34 @@ import {
   getTasks, addTask, updateTask, deleteTask, getEmployeeTasks, getEmployeeAttendance, getEmployeePerformance,
   getEmployeeById, getCurrentEmployee, updateProfile
 } from "../controllers/employeeController.js"; // Make sure the path is correct
-
+import { protectEmployee } from "../middleware/authEmployee.js";
+import { protect } from "../middleware/auth.js";
 const router = Router();
 
 // PUBLIC ROUTES - NO PROTECTION
 router.post("/login", login);
 
 // EMPLOYEE ROUTES - NO PROTECTION
-router.get("/me", getCurrentEmployee);
-router.patch("/me/change-password", changePassword);
-router.patch("/update-profile", updateProfile);
-router.get("/task", getTasks);
-router.post("/task", addTask);
-router.patch("/task/:id", updateTask);
-router.delete("/task/:id", deleteTask);
-router.get("/employee/:id/tasks", getTasks);
-router.get("/employee/:id/", getEmployeeById);
+router.get("/me",protectEmployee, getCurrentEmployee);
+router.patch("/me/change-password",protectEmployee, changePassword);
+router.patch("/update-profile",protectEmployee, updateProfile);
+router.get("/task",protectEmployee, getTasks);
+router.post("/task",protectEmployee, addTask);
+router.patch("/task/:id",protectEmployee, updateTask);
+router.delete("/task/:id", protectEmployee,deleteTask);
+router.get("/employee/:id/tasks",protectEmployee, getTasks);
+router.get("/employee/:id/",protectEmployee, getEmployeeById);
 
 // ADMIN ROUTES - NO PROTECTION
-router.get("/get/employee", getAll);
-router.post("/create/employee", createEmployee);
-router.patch("/update/:id", updateEmployee);
-router.delete("/delete/:id", deleteEmployee);
-router.post("/:id/reset-password", resetPassword);
-router.get("/:id/password", getEmployeePassword);
-router.get("/admin/employee/:id/tasks", getEmployeeTasks);
-router.get("/employee/:id/performance", getEmployeePerformance);
-router.get("/:id/tasks", getEmployeeTasks);
-router.get("/:id/attendance", getEmployeeAttendance);
+router.get("/get/employee",protect, getAll);
+router.post("/create/employee",protect, createEmployee);
+router.patch("/update/:id",protect, updateEmployee);
+router.delete("/delete/:id",protect, deleteEmployee);
+router.post("/:id/reset-password",protect, resetPassword);
+router.get("/:id/password",protect, getEmployeePassword);
+router.get("/admin/employee/:id/tasks",protect, getEmployeeTasks);
+router.get("/employee/:id/performance",protect, getEmployeePerformance);
+router.get("/:id/tasks",protect, getEmployeeTasks);
+router.get("/:id/attendance",protect, getEmployeeAttendance);
 
 export default router;
