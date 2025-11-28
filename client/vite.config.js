5f16da2216fab38cd0e0a4dbdf1f3a733483ea5d
YadneshUmredkar75 <yadneshUmredkar75@gmail.com>
2025-11-28 11:21:15 +0530

    build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // Important for deployment (fixes MIME & vite.svg issue)
+  base: './',  
+
   server: {
     proxy: {
-      // All /api requests go to your backend
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
-        // Optional: rewrite /api → /
-        // rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-});
+})
