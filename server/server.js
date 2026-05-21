5bc8c1d594bd2e05b0e889219ac53f73944eabc7
YadneshUmredkar75 <yadneshumredkar75@gmail.com>
2026-05-21 17:29:32 +0530

    downgrade express to v4

--- server/server.js
+++ server/server.js
@@ -120,7 +120,7 @@ app.use((err, req, res, next) => {
 });
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Backend Server Running on Port: ${PORT}`);
 });
